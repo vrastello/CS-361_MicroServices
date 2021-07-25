f174e1790094734736601511e8ef54bbb481c325
vrastello <vrastello11@gmail.com>
2021-07-25 11:35:12 -0700

    Added login and features

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,4 @@
+# frozen_string_literal: true
+
 class MainController < ApplicationController
-    def index
-      flash[:notice] = "Logged in successfully"
-      flash[:alert] = "Invalid username or password"
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,17 @@ Rails.application.routes.draw do
   # GET /about
   get "/about", to: "about#index"
 
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
+
   root to: "main#index"
   
   
