c79d88e12d3f208bf6c80fd91cd34f929b52a2d2
vrastello <vrastello11@gmail.com>
2021-07-24 17:56:07 -0700

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+Rails.application.routes.draw do
+  # GET /about
+  get "/about", to: "about#index"
+
+  root to: "main#index"
+  
+  
+
+end
